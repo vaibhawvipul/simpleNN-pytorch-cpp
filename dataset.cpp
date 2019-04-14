b274a21b45cb7e16cdf0da522d1787bbfa9ea3e5
vaibhawvipul <vaibhaw.vipul@gmail.com>
2019-04-14 09:33:55 +0530

    init commits

---
+++ dataset.cpp
@@ -0,0 +1,95 @@
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+#include <initializer_list>
+#include "dataset.h"
+
+void DataSet::ReadCSVFile(std::string file_name) {
+  
+  std::ifstream file(file_name.c_str());
+  if (!file) {
+    file.open(file_name.c_str());
+  }
+  if (!file) {
+    std::cerr << "ERROR: No " << file_name << " found." << std::endl;
+  }
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string line;
+  std::vector<std::string> lines;
+  while(getline(buffer, line, '\n')) {
+    lines.push_back(line);
+    //std::cout << line << std::endl;
+  }
+
+  // the other lines contain the features for each flower
+  for (int i = 1; i < lines.size(); ++i) {
+    std::vector<float> features = ReadCSVLine(lines[i]);
+    //std::cout << "line is : " << lines[i] << std::endl;
+    //std::cout << "feature[0] is : " << features[0] << std::endl;
+    //std::cout << "feature[1] is : " << features[1] << std::endl;
+    //std::cout << "feature[2] is : " << features[2] << std::endl;
+    //std::cout << "feature[3] is : " << features[3] << std::endl;
+    //std::cout << "feature[4] is : " << features[4] << std::endl;
+      x1_.push_back(features[0]);
+      x2_.push_back(features[1]);
+      x3_.push_back(features[2]);
+      x4_.push_back(features[3]);
+      if (features[4] == 1) {
+        std::vector<float> y_feature;
+        y_feature.push_back(1.0f);
+        y_feature.push_back(0.0f);
+        y_.push_back(y_feature);
+      } else if (features[4] == 0.5f){
+        std::vector<float> y_feature;
+        y_feature.push_back(0.0f);
+        y_feature.push_back(1.0f);
+        y_feature.push_back(0.0f);
+        y_.push_back(y_feature);
+      } else {
+        std::vector<float> y_feature;
+        y_feature.push_back(0.0f);
+        y_feature.push_back(0.0f);
+        y_feature.push_back(1.0f);
+        y_.push_back(y_feature);
+      }
+  }
+}
+
+std::vector<float> DataSet::ReadCSVLine(std::string line) {
+  std::vector<float> line_data;
+  std::stringstream lineStream(line);
+  std::string cell;
+  while(std::getline(lineStream, cell, ','))
+  {
+    if (cell!="setosa" && cell!="versicolor" && cell!="virginica") {
+      line_data.push_back(std::stod(cell.c_str()));
+    } else{
+      if (cell=="setosa"){
+        line_data.push_back(1.0f);
+      } 
+      else if (cell == "versicolor") {
+        line_data.push_back(0.5f);
+      } else {
+        line_data.push_back(0.0f);
+      }
+    }
+  }
+  return line_data;
+}
+
+std::initializer_list<float> DataSet::input(float petal_length, float petal_width, float sepal_length, float sepal_width) {
+  return {petal_length,petal_width,sepal_length,sepal_width};
+}
+
+std::string DataSet::output(std::vector<float> one_hot_encoding_species) {
+  if (one_hot_encoding_species[0]==1){
+    return "setosa";
+  } else if (one_hot_encoding_species[1]==1){
+    return "versicolor";
+  } else {
+    return "virginica";
+  }
+}
---
+++ simpleNN.cpp
@@ -0,0 +1,75 @@
+#include <torch/script.h>
+#include <torch/torch.h>
+#include <iostream>
+#include <vector>
+#include "dataset.h"
+
+struct Net : torch::nn::Module {
+  Net() {
+    // Construct and register two Linear submodules.
+    fc1 = register_module("fc1", torch::nn::Linear(4, 10));
+    fc2 = register_module("fc2", torch::nn::Linear(10, 3));
+  }
+
+  // Implement the Net's algorithm.
+  torch::Tensor forward(at::Tensor x) {
+    // Use one of many tensor manipulation functions.
+    x = torch::relu(fc1->forward(x.reshape(x.size(0))));
+    x = fc2->forward(x);
+    return x;
+  }
+
+  // Use one of many "standard library" modules.
+  torch::nn::Linear fc1{nullptr}, fc2{nullptr};
+};
+
+int main() {
+
+  //loading data
+  DataSet data_set("/home/vipul/Documents/vaibhawvipul/DeepLearning/simpleNNPytorchCpp/iris.csv");
+
+  //loading model 
+  auto net = std::make_shared<Net>();
+
+  //setting up optimizer
+  torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
+
+  for (size_t epoch = 1; epoch <= 1; ++epoch) {
+
+    // Iterate the data loader to yield batches from the dataset.
+    for (unsigned i = 0; i < data_set.x1().size(); i++) {
+
+      //form an input tensor combined of x1, x2, x3 and x4
+      std::vector<float> input_vector;
+      input_vector.push_back(data_set.x1()[i]);
+      input_vector.push_back(data_set.x2()[i]);
+      input_vector.push_back(data_set.x3()[i]);
+      input_vector.push_back(data_set.x4()[i]);
+
+      at::Tensor input_tensor = at::from_blob(input_vector.data(),{long(input_vector.size()),1});
+      input_tensor = input_tensor.toType(at::kFloat);
+
+      std::vector<float> target_output;
+      target_output = data_set.y()[i];
+
+      at::Tensor output_tensor = at::from_blob(target_output.data(),{long(target_output.size()),1});
+      output_tensor = output_tensor.toType(at::kFloat);
+
+      // Reset gradients.
+      optimizer.zero_grad();
+      // Execute the model on the input data.
+      torch::Tensor prediction = net->forward(input_tensor);
+      // Compute a loss value to judge the prediction of our model.
+      torch::Tensor loss = torch::nll_loss(prediction,output_tensor);
+      // Compute gradients of the loss w.r.t. the parameters of our model.
+      loss.backward();
+      // Update the parameters based on the calculated gradients.
+      optimizer.step();
+      // Output the loss and checkpoint every 100 batches.
+      std::cout << "Epoch: " << epoch << " | Loss: " << loss.item<float>() << std::endl;
+      // Serialize your model periodically as a checkpoint.
+      torch::save(net, "net.pt");
+    }
+  }
+  return 0;
+}
